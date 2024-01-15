717a1f72712732b6a6f8fd0a1fa3d9cf03b23ac8
bderembl <bruno.deremble@cnrs.fr>
2024-01-15 11:33:35 +0100

    only two temporary arrays for the ffts

--- src/elliptic.h
+++ src/elliptic.h
@@ -8,9 +8,8 @@
 
 
 // FFTW in/outputs and plans
-double *in1;
-double *out1; 
-double *out2; 
+double *wrk1;
+double *wrk2;
 
 fftw_plan transfo_direct, transfo_inverse;
 
@@ -47,14 +46,13 @@ void init_elliptic(){
   alloc_local = fftw_mpi_local_size_2d(Nym1, Nxm1, MPI_COMM_WORLD,
                                        &local_n0, &local_0_start);
   
-  in1 = fftw_alloc_real(alloc_local);
-  out1 = fftw_alloc_real(alloc_local);
-  out2 = fftw_alloc_real(alloc_local);
+  wrk1 = fftw_alloc_real(alloc_local);
+  wrk2 = fftw_alloc_real(alloc_local);
   
   /* create plan for out-of-place */
-  transfo_direct = fftw_mpi_plan_r2r_2d(Nym1, Nxm1, in1, out1, MPI_COMM_WORLD,
+  transfo_direct = fftw_mpi_plan_r2r_2d(Nym1, Nxm1, wrk1, wrk2, MPI_COMM_WORLD,
                                         FFTW_RODFT00, FFTW_RODFT00, FFTW_EXHAUSTIVE|FFTW_DESTROY_INPUT|FFTW_MPI_TRANSPOSED_OUT);
-  transfo_inverse = fftw_mpi_plan_r2r_2d(Nym1, Nxm1, out1, out2, MPI_COMM_WORLD,
+  transfo_inverse = fftw_mpi_plan_r2r_2d(Nym1, Nxm1, wrk2, wrk1, MPI_COMM_WORLD,
                                          FFTW_RODFT00, FFTW_RODFT00, FFTW_EXHAUSTIVE|FFTW_DESTROY_INPUT|FFTW_MPI_TRANSPOSED_IN);
   
   J0 = local_0_start + 1; // member the fourier grid starts at the index 1 of the real space grid
@@ -66,12 +64,11 @@ void init_elliptic(){
 
   J0 = 1;
   
-  in1  = calloc( Nxm1*Nym1, sizeof( double ) );
-  out1 = calloc( Nxm1*Nym1, sizeof( double ) );
-  out2 = calloc( Nxm1*Nym1, sizeof( double ) );
+  wrk1  = calloc( Nxm1*Nym1, sizeof( double ) );
+  wrk2 = calloc( Nxm1*Nym1, sizeof( double ) );
   
-  transfo_direct  = fftw_plan_r2r_2d(Nym1,Nxm1, in1, out1, FFTW_RODFT00, FFTW_RODFT00, FFTW_EXHAUSTIVE|FFTW_DESTROY_INPUT);
-  transfo_inverse = fftw_plan_r2r_2d(Nym1,Nxm1, out1, out2, FFTW_RODFT00, FFTW_RODFT00, FFTW_EXHAUSTIVE|FFTW_DESTROY_INPUT);
+  transfo_direct  = fftw_plan_r2r_2d(Nym1,Nxm1, wrk1, wrk2, FFTW_RODFT00, FFTW_RODFT00, FFTW_EXHAUSTIVE|FFTW_DESTROY_INPUT);
+  transfo_inverse = fftw_plan_r2r_2d(Nym1,Nxm1, wrk2, wrk1, FFTW_RODFT00, FFTW_RODFT00, FFTW_EXHAUSTIVE|FFTW_DESTROY_INPUT);
   
 #endif
   
@@ -96,10 +93,10 @@ void invert_pv(double *q, double *psi) {
     
     for(int j = 1; j<Ny; j++){
       for(int i = 1; i <Nx; i++){
-        in1[idx_fft(i,j)] = 0;
+        wrk1[idx_fft(i,j)] = 0;
         // inner loop on layers: projection on modes
         for (int l = 0; l < nl ; l++) {
-          in1[idx_fft(i,j)] += cl2m[k*nl+l]*q[idx(i,j,l)];
+          wrk1[idx_fft(i,j)] += cl2m[k*nl+l]*q[idx(i,j,l)];
         }
       }
     }
@@ -111,7 +108,7 @@ void invert_pv(double *q, double *psi) {
     for(int j = 1; j < Ny; j++){ // f = -g / ( kx^2 + ky^2 + Rd^2)
       for (int i = 1; i < Nx; i++){
         double fact = - (sq(L[i]) + sq(K[j]) + iRd2[k]);
-        out1[idx_fft(i,j)] = out1[idx_fft(i,j)]/fact;
+        wrk2[idx_fft(i,j)] = wrk2[idx_fft(i,j)]/fact;
       }
     }
 
@@ -121,7 +118,7 @@ void invert_pv(double *q, double *psi) {
     // Normalisation (different for MPI as we need to take the global Nyt)
     for(int j = 1; j<Ny; j++){
       for(int i = 1;i <Nx; i++){
-        out2[idx_fft(i,j)] = out2[idx_fft(i,j)]/(4*(Nxm1 + 1)*NY);
+        wrk1[idx_fft(i,j)] = wrk1[idx_fft(i,j)]/(4*(Nxm1 + 1)*NY);
       }
     }
 
@@ -129,7 +126,7 @@ void invert_pv(double *q, double *psi) {
       for(int i = 1;i <Nx; i++){
         // loop on layer: populating psi (layer l)
         for (int l = 0; l < nl ; l++) {
-          psi[idx(i,j,l)] += cm2l[l*nl+k]*out2[idx_fft(i,j)];
+          psi[idx(i,j,l)] += cm2l[l*nl+k]*wrk1[idx_fft(i,j)];
         }
       }
     }
@@ -142,9 +139,8 @@ void invert_pv(double *q, double *psi) {
 
 void clean_fft(){
 
-  free(in1);
-  free(out1); 
-  free(out2); 
+  free(wrk1);
+  free(wrk2); 
   
   fftw_destroy_plan(transfo_direct); 
   fftw_destroy_plan(transfo_inverse);
